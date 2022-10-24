ba590342519a3a339c9f67ca41dce022a1eaf3f0
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-10-24 14:01:46 +0200

    Updated unused variable

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,7 +1,11 @@
 import joker from 'give-me-a-joke';
 
 export function dadJoke() {
-  return new Promise(function (resolve, reject) {
-    joker.getRandomDadJoke(resolve);
-  });
+  try {
+    return new Promise(function (resolve, reject) {
+      joker.getRandomDadJoke(resolve);
+    });
+  } catch (error) {
+    reject(error);
+  }
 }
