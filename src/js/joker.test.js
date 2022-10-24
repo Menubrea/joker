7124eb4d1c131229d1cdff2c44d720e66d266b89
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-10-24 13:50:54 +0200

    Updated scripts to singlequotes

--- src/js/joker.test.js
+++ src/js/joker.test.js
@@ -1,10 +1,10 @@
-import { dadJoke } from "./joker.js";
+import { dadJoke } from './joker.js';
 
 export async function testDadJoke() {
   const joke = await dadJoke();
   const testPasses = Boolean(joke);
   console.log(joke);
-  console.log("Dad Joke Test:", testPasses ? "Passed" : "FAILED");
+  console.log('Dad Joke Test:', testPasses ? 'Passed' : 'FAILED');
   return testPasses;
 }
 
