cc56ba0b4581817f654c1226cf0c738819ac65c2
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-10-24 13:37:32 +0200

    Added new test script for dadjoke

---
+++ src/js/joker.test.js
@@ -0,0 +1,11 @@
+import { dadJoke } from "./joker.js";
+
+export async function testDadJoke() {
+  const joke = await dadJoke();
+  const testPasses = Boolean(joke);
+  console.log(joke);
+  console.log('Dad Joke Test:', testPasses ? 'Passed' : 'FAILED');
+  return testPasses;
+}
+
+testDadJoke();
