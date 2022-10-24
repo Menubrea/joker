2ef870f1d57248c0b2c269423d635cfbe38f361d
ClovesPirate <91755385+ClovesPirate@users.noreply.github.com>
2022-10-24 13:48:39 +0200

    Updated format script and ran format script on supported files/folders

--- src/js/joker.test.js
+++ src/js/joker.test.js
@@ -4,7 +4,7 @@ export async function testDadJoke() {
   const joke = await dadJoke();
   const testPasses = Boolean(joke);
   console.log(joke);
-  console.log('Dad Joke Test:', testPasses ? 'Passed' : 'FAILED');
+  console.log("Dad Joke Test:", testPasses ? "Passed" : "FAILED");
   return testPasses;
 }
 
